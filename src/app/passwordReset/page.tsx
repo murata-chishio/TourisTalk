34b2787c1330da9d780c875154627383312d47f4
murata-chishio <murata.chishio646@mail.kyutech.jp>
2024-05-21 00:34:22 +0900

    error解消

--- src/app/passwordReset/page.tsx
+++ src/app/passwordReset/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { supabase } from "@/utils/supabase";
 import { useState } from "react";
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 export default function PasswordReset(){
   const router = useRouter();
